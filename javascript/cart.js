2c3e8794eea5da45ece6a83a6e7b151fbe63ebce
tiffany1029tw <tiffany1029tw@gmail.com>
2023-12-18 10:58:43 +0800

    update

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,63 +1,59 @@
-document.addEventListener("DOMContentLoaded", function () { //先抓需要的元素
-    const checkboxes = document.querySelectorAll('[type="checkbox"]');
-    const countSelects = document.querySelectorAll('[name^="count"]');
-    const priceElements = document.querySelectorAll('.price1');
-    const totalElement = document.querySelector('.total_price');
-    const cancelButtons = document.querySelectorAll('.cancel_btn button');
+document.addEventListener("DOMContentLoaded", function () { 
+//先抓需要的元素
+    const checkboxes = document.querySelectorAll('[type="checkbox"]'); //全選的選取框框
+    const countSelects = document.querySelectorAll('[name^="count"]'); //找count開頭的元素 count1
+    const priceElements = document.querySelectorAll('.price1'); //找商品單價的元素
+    const totalElement = document.querySelector('.total_price'); //找合計總金額的元素
+    const cancelButtons = document.querySelectorAll('.cancel_btn button'); //找刪除商品的按鈕
 
-    // Function to update the total price
-    function updateTotal() {
+    function updateTotal() { // 更新總金額的功能
         let totalPrice = 0;
         priceElements.forEach((priceElement, index) => {
-            if (checkboxes[index].checked) {
+            if (checkboxes[index].checked) { //選取框被選取後
                 const count = parseInt(countSelects[index].value, 10);
                 const price = parseInt(priceElement.textContent.replace('NT$', ''), 10);
                 totalPrice += count * price;
             }
         });
-        totalElement.textContent = `NT$${totalPrice}`;
+        totalElement.textContent = `NT$${totalPrice}`; //寫出總金額
     }
 
-    // Function to handle checkbox change
-    function handleCheckboxChange() {
-        updateTotal();
+    function checkboxChange() { // 控制選取框
+        updateTotal(); //更新總金額
     }
 
-    // Function to handle select change
-    function handleSelectChange() {
-        updateTotal();
+    function selectChange() { //控制選單的
+        updateTotal(); 
     }
 
-    // Function to handle cancel button click
-    function handleCancelButtonClick(index) {
-        // Remove the corresponding product row
-        checkboxes[index].closest('.dtls').remove();
-        updateTotal();
+    function cancelButton(index) { //控制刪除按鈕
+        checkboxes[index].closest('.dtls').remove(); 
+        //確認選取框是否被選取到 若有選取就刪去整行的元素(父層是.dtls)
+        updateTotal(); 
     }
 
-    // Function to handle "全選" checkbox
-    function handleSelectAllCheckboxChange() {
-        const isChecked = checkboxes[0].checked;
+    function selectAllCheckboxChange() { //全選選取框功能
+        const isChecked = checkboxes[0].checked; //是否有選到
         checkboxes.forEach((checkbox, index) => {
             if (index !== 0) {
-                checkbox.checked = isChecked;
+                checkbox.checked = isChecked; //所有選取框都被選到的狀態就代表全選
             }
         });
         updateTotal();
     }
 
-    // Attach event listeners
+    // 增加每個的事件監聽器
     checkboxes.forEach((checkbox, index) => {
-        checkbox.addEventListener('change', handleCheckboxChange);
-    });
+        checkbox.addEventListener('change', checkboxChange);
+    }); //選取框被選中
 
     countSelects.forEach(select => {
-        select.addEventListener('change', handleSelectChange);
-    });
+        select.addEventListener('change', selectChange);
+    }); //選單的數字有變
 
     cancelButtons.forEach((button, index) => {
-        button.addEventListener('click', () => handleCancelButtonClick(index));
-    });
+        button.addEventListener('click', () => cancelButton(index));
+    }); //刪除的按鈕被點按
 
-    checkboxes[0].addEventListener('change', handleSelectAllCheckboxChange);
+    checkboxes[0].addEventListener('change', selectAllCheckboxChange);
 });
